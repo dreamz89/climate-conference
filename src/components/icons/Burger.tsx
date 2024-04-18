e76a64f75ebb0e55dec33aed33b1a6492c327e7a
Jocelyn <jocelyntan89@hotmail.com>
2024-04-18 17:24:46 +0200

    feat: add icons

---
+++ src/components/icons/Burger.tsx
@@ -0,0 +1,11 @@
+const Burger = () => {
+  return (
+    <>
+      <rect y="11" width="24" height="2" rx="1" />
+      <rect y="4" width="24" height="2" rx="1" />
+      <rect y="18" width="24" height="2" rx="1" />
+    </>
+  )
+}
+
+export default Burger
