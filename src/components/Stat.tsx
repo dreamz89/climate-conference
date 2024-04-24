adf3a63df7dea59cc679516de156d4bcf0daded2
Jocelyn <jocelyntan89@hotmail.com>
2024-04-24 23:18:03 +0200

    fix: styling

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -3,7 +3,7 @@ import { storyblokEditable } from "@storyblok/react/rsc"
 const Stat = ({ blok }: any) => {
   return (
     <div {...storyblokEditable(blok)}>
-      <b className="text-[2rem] lg:text-[3rem]">{blok.number}</b>
+      <b className="block text-[1.75rem] lg:mb-1 lg:text-4xl">{blok.number}</b>
       <p className="text-sm lg:text-[1.25rem]">{blok.label}</p>
     </div>
   )
