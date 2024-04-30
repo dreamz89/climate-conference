e14d8e49f961d9fa7e1ab8fb047b20c657497165
Jocelyn <jocelyntan89@hotmail.com>
2024-04-30 16:12:45 +0200

    fix: external links must have https

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,17 +2,22 @@ import { storyblokEditable } from "@storyblok/react/rsc"
 
 const Button = ({ blok }: any) => {
   if (blok.link.url) {
+    // external link
+    const url = blok.link.url
+
     return (
       <a
         className="flex w-40 justify-center rounded bg-yellow-400 py-3 leading-4 hover:bg-yellow-500"
-        href={blok.link.url}
+        href={url.startsWith("http") ? url : `https://${url}`}
         target="_blank"
+        rel="noopener noreferrer nofollow"
         {...storyblokEditable(blok)}
       >
         {blok.label}
       </a>
     )
   } else if (blok.link.story) {
+    // internal link
     const url =
       blok.link.story?.full_slug === "home"
         ? "/"
